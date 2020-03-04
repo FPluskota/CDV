34ea5801c8ab4efc533e784be353ebf87e2073b8
FPluskota <61701532+FPluskota@users.noreply.github.com>
2020-03-04 15:28:39 +0100

    Update 1_podstawa.py

--- Struktularne/1_podstawa.py
+++ Struktularne/1_podstawa.py
@@ -1,1 +1,42 @@
 print("CDV")
+print(10)
+'''
+komentarz
+blokowy
+'''
+#potęgowanie
+potega=2 ** 10
+print (potega)
+
+#pobieranie danych z klawiatury
+name=input ()
+surname= input ()
+# +konkatenacja
+print("Twoje imię: "+name)
+print("Twoje imię: "+name +"nazwisko: "+surname)
+
+'''
+Uzytkownik podaje dane z klawiatury swój wiek
+wysletl dane w formacie: twój wiek lat
+'''
+
+print("podaj swój wiek: ", end="")
+age= input()
+print("Twój wiek: ",age, "lat")
+
+age1=20
+print(type(age1))
+
+surname="Kowalski"
+firstLetter=surname[0]
+print (firstLetter)
+
+lenght = len(surname)
+print(lenght)
+
+lastLetter=surmane[len(surname)-1]
+print(lastLetter)
+
+lastLetter=surname[-1]
+print(lastLetter)
+
